898f2ac3f61fa54c31134c8623c2f1e35a424bfb
Student <student@eda-bc-05.localdomain>
2020-02-14 09:32:32 +1300

    catFact button added not working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
 
 import CatFacts from './CatFacts'
 import SavedFacts from './SavedFacts'
 
 const App = () => (
-  <Router>
+ 
     <div className='app'>
-      <Route exact path='/' component={CatFacts} />
-      <Route path='/savedfacts' component={SavedFacts} />
+      <CatFacts />
     </div>
-  </Router>
 )
 
 export default App
--- client/components/SavedFacts.jsx
+++ client/components/SavedFacts.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 import { connect } from 'react-redux'
 // import your action function
-import { savedFacts } from '../actions'
+
 
 class CatFacts extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
   render() {
     return (
