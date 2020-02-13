85d80a755cdede4ddb0ba026503e51cbaaf9e9f2
Student <student@eda-bc-05.localdomain>
2020-02-14 11:34:27 +1300

    merging

--- client/components/CatFacts.jsx
+++ client/components/CatFacts.jsx
@@ -16,9 +16,10 @@ class CatFacts extends React.Component {
       <div>
         <h1>Catscan</h1>
         <div>
-        <button className='btn btn-warning' onClick={saveFact}>Save my fact</button>
+          <button className='btn btn-warning' onClick={() => this.props.dispatch(saveFact(this.props.catFact))}>Save my fact</button>
           <button className='btn btn-warning'>Next fact</button>
         </div>
+
         <div>
           <button className='btn btn-warning' onClick={() => this.props.dispatch(getSavedFacts())}>Get me cat facts</button>
           {this.props.savedFacts.map(fact => <li>{fact.cat_fact}</li>)}
