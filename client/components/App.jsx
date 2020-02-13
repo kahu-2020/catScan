5acf1515d51f052c79fdce04b2e40a11cd443eae
Student <student@eda-bc-05.localdomain>
2020-02-13 17:01:23 +1300

    initial commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 
-import ErrorMessage from './ErrorMessage'
-import LoadSubreddit from './LoadSubreddit'
-import SubredditList from './SubredditList'
-import WaitIndicator from './WaitIndicator'
+import CatFacts from './CatFacts'
+import SavedList from './SavedList'
 
 const App = () => (
   <div className='app'>
-    <ErrorMessage />
-    <LoadSubreddit>
-      <WaitIndicator />
-    </LoadSubreddit>
-    <SubredditList />
+    <CatFacts />
+      <SavedList /> 
+       {/* props.children */}
   </div>
 )
 
---
+++ client/components/CatFacts.jsx
@@ -0,0 +1,32 @@
+import react from 'react'
+
+import { connect } from 'react-redux'
+// import your action function
+
+class CatFacts extends React.Component {
+
+  // state = { }
+
+
+
+
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+
+
+
+    )
+  }
+
+}
+
+// mapStateToProps(){
+
+// }
+
+export default connect(mapStateToProps)(CatFacts)
