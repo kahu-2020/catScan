506b60ecb7a61695c10e96706f65095561aa930e
Student <student@eda-bc-05.localdomain>
2020-02-14 12:09:28 +1300

    to push

--- client/components/CatFacts.jsx
+++ client/components/CatFacts.jsx
@@ -19,11 +19,11 @@ class CatFacts extends React.Component {
         <p>{this.props.catFact}</p>
         <div>
           <button className='btn btn-warning' onClick={() => this.props.dispatch(saveFact(this.props.catFact))}>Save my fact</button>
-          <button className='btn btn-warning'>Next fact</button>
+          <button className='btn btn-warning'  onClick={() => {this.props.dispatch(fetchCatFacts())}}>Next fact</button>
         </div>
 
         <div>
-          <button className='btn btn-warning' onClick={() => this.props.dispatch(getSavedFacts())}>Get me cat facts</button>
+          <button className='btn btn-warning' >Get me cat facts</button>
           {this.props.savedFacts.map(fact => <li>{fact.cat_fact}</li>)}
         </div>
       </div>
