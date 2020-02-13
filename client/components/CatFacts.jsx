25b9327ee2442705d2e609539b5d95edf6f57a48
claire-russell <russell.claireolivia@gmail.com>
2020-02-13 20:53:19 +1300

    components added

--- client/components/CatFacts.jsx
+++ client/components/CatFacts.jsx
@@ -1,5 +1,5 @@
-import react from 'react'
-
+import React from 'react'
+import Link from 'react-router-dom'
 import { connect } from 'react-redux'
 // import your action function
 
@@ -8,25 +8,22 @@ class CatFacts extends React.Component {
   // state = { }
 
 
-
-
-
-
   render() {
     return (
       <div>
-
+        <h1>Catscan</h1>
+        <div>
+        <button className='btn btn-warning'>Save my fact</button>
+        <button className='btn btn-warning'>Next fact</button>
+        </div>
+        <button className='btn btn-warning' >View my cat facts</button>  {/* <SavedFacts />  */}
       </div>
-
-
-
     )
   }
-
 }
 
 // mapStateToProps(){
 
 // }
 
-export default connect(mapStateToProps)(CatFacts)
+export default connect()(CatFacts)
---
+++ client/reducers/catFacts.js
@@ -0,0 +1,11 @@
+
+function catFacts (state = '', action) {
+  switch (action.type) {
+    // case RECEIVE_CATFACTS:
+    //   return action.posts
+    default:
+      return state
+  }
+}
+
+export default catFacts
